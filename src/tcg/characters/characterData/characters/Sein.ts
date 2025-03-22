6712bbed62841f7bf1a67227b93e7da4eebbbecd
HxHexa <tomdale123456@gmail.com>
2025-03-22 13:14:57 -0400

    add sein card images from vangerisu

--- src/tcg/characters/characterData/characters/Sein.ts
+++ src/tcg/characters/characterData/characters/Sein.ts
@@ -13,7 +13,8 @@ const SEIN_HEALING_RAMP = 0.1;
 
 const imageUrl: Record<string, string> = {
   icon: "https://cdn.discordapp.com/attachments/1346555621952192522/1347898000717910057/Sein_anime_portrait.webp?ex=67dca896&is=67db5716&hm=ce78236ebb64724705c48a5221039f22e546cd1c9f940aa0036003b8bc74e49b&",
-  vangerisuCardVer: "",
+  vangerisuCardVer:
+    "https://cdn.discordapp.com/attachments/1351391350398128159/1353035306840096848/Sein_Card.png?ex=67e02fd3&is=67dede53&hm=53829f9cd04a268eb8b6f9dba932220e814be02c9c6f6e76f61ab83e6c20c24d&",
 };
 
 const seinStats = new Stats({
@@ -34,7 +35,7 @@ export const createSein = () =>
       },
       emoji: CharacterEmoji.SEIN,
       color: 0xa3caca,
-      imageUrl: imageUrl.icon,
+      imageUrl: imageUrl.vangerisuCardVer,
     },
     get stats() {
       const characterStats: any = getStats();
--- src/tcg/decks/SeinDeck.ts
+++ src/tcg/decks/SeinDeck.ts
@@ -11,6 +11,10 @@ export const a_trustInYourAllyFrierensZoltraak = new Card({
   title: "Trust in Your Ally: Frieren's Zoltraak",
   description: ([dmg]) => `HP-5. DMG ${dmg} + HP/10`,
   emoji: CardEmoji.SEIN_CARD,
+  cosmetic: {
+    cardImageUrl:
+      "https://cdn.discordapp.com/attachments/1351391350398128159/1353035311357362268/Trust_in_your_Ally_Frierens_Zoltraak_1.png?ex=67e02fd4&is=67dede54&hm=6cb8d375f497cf7bca2637de8d70a900901116aa53f0b0aa3e977ddc27a5def8&",
+  },
   effects: [5],
   cardAction: function (this: Card, game, characterIndex, messageCache) {
     const character = game.characters[characterIndex];
@@ -38,6 +42,10 @@ export const a_trustInYourAllyFernsBarrage = new Card({
   description: ([dmg]) =>
     `HP-7. DMG ${dmg}+HP/10 DMG. Next turn, deal ${dmg}+HP/10 DMG at turn end.`,
   emoji: CardEmoji.SEIN_CARD,
+  cosmetic: {
+    cardImageUrl:
+      "https://cdn.discordapp.com/attachments/1351391350398128159/1353035310996394024/Trust_in_your_Ally_Ferns_Barrage.png?ex=67e02fd4&is=67dede54&hm=57cae7a03eefb0dacaa8649910e48ab411d50ca94a9855b078098e751b92ae75&",
+  },
   effects: [3],
   cardAction: function (this: Card, game, characterIndex, messageCache) {
     const character = game.characters[characterIndex];
@@ -87,6 +95,10 @@ const a_trustInYourAllyStarksLightningStrike = new Card({
   title: "Trust in Your Ally: Stark's Lightning Strike",
   description: ([dmg]) => `Priority-1. HP-9. DMG ${dmg}+HP/7.`,
   emoji: CardEmoji.SEIN_CARD,
+  cosmetic: {
+    cardImageUrl:
+      "https://cdn.discordapp.com/attachments/1351391350398128159/1353035310677622845/Trust_in_your_Ally_Starks_Lightning_Strike.png?ex=67e02fd4&is=67dede54&hm=608a0bc2795f44b1512ecb7d26b29213aedada2f9f9db64b178447be0dd98476&",
+  },
   effects: [7],
   priority: -1,
   cardAction: function (this: Card, game, characterIndex, messageCache) {
@@ -117,6 +129,10 @@ export const mugOfBeer = new Card({
   title: "Mug of Beer",
   description: ([hp, atk]) => `HP+${hp}. ATK+${atk}. DEF-2. SPD-1.`,
   emoji: CardEmoji.SEIN_CARD,
+  cosmetic: {
+    cardImageUrl:
+      "https://cdn.discordapp.com/attachments/1351391350398128159/1353038477805228073/Mug_of_Beer_2.png?ex=67e032c7&is=67dee147&hm=9e453e019c32d60027135834549e42bd19f16995569570a3012b9626a5fdf6f4&",
+  },
   effects: [6, 2],
   cardAction: function (this: Card, game, characterIndex, messageCache) {
     const character = game.characters[characterIndex];
@@ -142,6 +158,10 @@ export const smokeBreak = new Card({
   title: "Smoke Break",
   description: ([atk, def, spd]) => `HP-5. ATK+${atk}. DEF+${def}. SPD+${spd}.`,
   emoji: CardEmoji.SEIN_CARD,
+  cosmetic: {
+    cardImageUrl:
+      "https://cdn.discordapp.com/attachments/1351391350398128159/1353035309906133052/Smoke_Break.png?ex=67e02fd4&is=67dede54&hm=a6eaadebd1ce83f74e2819c16eb7cb57e8fb0f9888f8083248ac49b54119ccf4&",
+  },
   effects: [3, 2, 2],
   cardAction: function (this: Card, game, characterIndex, messageCache) {
     const character = game.characters[characterIndex];
@@ -170,6 +190,10 @@ const awakening = new Card({
   title: "Awakening",
   description: ([atk, def, spd]) => `ATK+${atk}. DEF+${def}. SPD+${spd}.`,
   emoji: CardEmoji.SEIN_CARD,
+  cosmetic: {
+    cardImageUrl:
+      "https://cdn.discordapp.com/attachments/1351391350398128159/1353035309436375121/Awakening.png?ex=67e02fd3&is=67dede53&hm=48c7275f912f8927990fa0073b3f8a6c2e7279042e5bd8edfe1a925261a0e5b5&",
+  },
   effects: [2, 1, 2],
   cardAction: function (this: Card, game, characterIndex, messageCache) {
     const character = game.characters[characterIndex];
@@ -194,6 +218,10 @@ export const poisonCure = new Card({
   title: "Poison Cure",
   description: ([hp]) => `HP+${hp}.`,
   emoji: CardEmoji.SEIN_CARD,
+  cosmetic: {
+    cardImageUrl:
+      "https://media.discordapp.net/attachments/1351391350398128159/1353035308844974100/Poison_Cure.png?ex=67e02fd3&is=67dede53&hm=76216578c19115f3ddcd9bff1c497b0c7c2f9ee87152f02c4085d681dd0dc6ae&=&format=webp&quality=lossless&width=908&height=1160",
+  },
   effects: [10],
   cardAction: function (this: Card, game, characterIndex, messageCache) {
     const character = game.characters[characterIndex];
@@ -213,6 +241,10 @@ export const braceYourself = new Card({
   description: ([def]) =>
     `Priority+1. Increases DEF by ${def} until the end of the turn.`,
   emoji: CardEmoji.SEIN_CARD,
+  cosmetic: {
+    cardImageUrl:
+      "https://cdn.discordapp.com/attachments/1351391350398128159/1353035308182147102/Brace_Yourself.png?ex=67e02fd3&is=67dede53&hm=b4bcacc46cae5fcd120b76d666ef606186621d3edc230a851e09a77976fce8eb&",
+  },
   priority: 1,
   effects: [20],
   cardAction: function (this: Card, game, characterIndex, messageCache) {
@@ -250,6 +282,10 @@ export const a_threeSpearsOfTheGoddess = new Card({
   description: ([dmg]) =>
     `HP-15. At the next 3 turn ends, deal ${dmg}+HP/10 DMG.`,
   emoji: CardEmoji.SEIN_CARD,
+  cosmetic: {
+    cardImageUrl:
+      "https://cdn.discordapp.com/attachments/1351391350398128159/1353035307397681172/Three_Spears_of_the_Godess.png?ex=67e02fd3&is=67dede53&hm=671b75347795840779fe6e5007f8a0918b04e3b6a558b561ca9ce4b4c18694a8&",
+  },
   effects: [7],
   cardAction: function (this: Card, game, characterIndex, messageCache) {
     const character = game.characters[characterIndex];
