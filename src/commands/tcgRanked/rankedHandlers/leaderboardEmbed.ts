511927d19ef2be3874bf9c249aa602d18b28135e
steampunkWizard <elijahanelson2nd@gmail.com>
2025-04-09 17:12:09 -0400

    fix leaderboard titles for both characters and ranked

--- src/commands/tcgRanked/rankedHandlers/leaderboardEmbed.ts
+++ src/commands/tcgRanked/rankedHandlers/leaderboardEmbed.ts
@@ -25,7 +25,7 @@ export default async function leaderboardEmbed(props: {
 }): Promise<EmbedBuilder> {
   const { idsToPoints, leaderboard, isCharacterLeaderboard } = props;
 
-  const leaderboardTitle = `${isCharacterLeaderboard ? `${characterNameToEmoji[leaderboard as CharacterName]} ` : ""}${leaderboard} Global Leaderboard`;
+  const leaderboardTitle = `${isCharacterLeaderboard ? `${characterNameToEmoji[leaderboard as CharacterName]} ` : "Ranked"} Global Leaderboard`;
   const userLines = idsToPoints.map((idtoPoint, index) => {
     const { id, points } = idtoPoint;
     const rank = index + 1;
