766ed592be895449a4b94e15241e88e25f7ece7e
steampunkWizard <elijahanelson2nd@gmail.com>
2025-04-26 09:45:36 -0400

    format

--- src/tcg/gameContextProvider.ts
+++ src/tcg/gameContextProvider.ts
@@ -1,8 +1,8 @@
-import Game from "@src/tcg/game"
+import Game from "@src/tcg/game";
 import { MessageCache } from "@src/tcgChatInteractions/messageCache";
 import { TCGThread } from "@src/tcgChatInteractions/sendGameMessage";
 import { StatsEnum } from "@src/tcg/stats";
-import Character from "@src/tcg/character"
+import Character from "@src/tcg/character";
 import Card from "@src/tcg/card";
 import CommonCardAction from "./util/commonCardActions";
 
@@ -12,61 +12,75 @@ export type GameContext = ReturnType<typeof gameContextProvider>;
 const EMPOWER_BOOST = 0.1;
 
 const calculateEffectValue = (baseValue: number, empowerLevel: number) => {
-    return Number(
-        (baseValue * (1 + empowerLevel * EMPOWER_BOOST)).toFixed(2)
-    );
-}
+  return Number((baseValue * (1 + empowerLevel * EMPOWER_BOOST)).toFixed(2));
+};
 // #endregion
 
 /**
  * Provides convenience methods and properties for cardActions.
  */
-export default function gameContextProvider(this: Card, game: Game, characterIndex: number, messageCache: MessageCache) {
-    const changeStat = (target: Character, amount: number, stat: StatsEnum ) => {
-        target.adjustStat(amount, stat);
-    }
-    const changeStatWithEmpower = (target: Character, stat: StatsEnum, effectIndex: number) =>{
-        const empowered = calculateEffectValue(this.effects[effectIndex], this.empowerLevel);
-        target.adjustStat(empowered, stat);
-    }
+export default function gameContextProvider(
+  this: Card,
+  game: Game,
+  characterIndex: number,
+  messageCache: MessageCache
+) {
+  const changeStat = (target: Character, amount: number, stat: StatsEnum) => {
+    target.adjustStat(amount, stat);
+  };
+  const changeStatWithEmpower = (
+    target: Character,
+    stat: StatsEnum,
+    effectIndex: number
+  ) => {
+    const empowered = calculateEffectValue(
+      this.effects[effectIndex],
+      this.empowerLevel
+    );
+    target.adjustStat(empowered, stat);
+  };
 
+  const self = game.getCharacter(characterIndex);
+  const opponent = game.getCharacter(characterIndex - 1);
 
-    const self = game.getCharacter(characterIndex);
-    const opponent = game.getCharacter(characterIndex - 1);
+  const sendToGameroom = (message: string) => {
+    messageCache.push(message, TCGThread.Gameroom);
+  };
 
-    const sendToGameroom = (message: string) => {
-        messageCache.push(message, TCGThread.Gameroom);
-    }
+  const flatSelfStat = changeStat.bind(null, self);
+  const selfStat = changeStatWithEmpower.bind(null, self);
+  const flatOpponentStat = changeStat.bind(null, opponent);
+  const opponentStat = changeStatWithEmpower.bind(null, opponent);
 
-    const flatSelfStat = changeStat.bind(null, self);
-    const selfStat = changeStatWithEmpower.bind(null, self);
-    const flatOpponentStat = changeStat.bind(null, opponent);
-    const opponentStat = changeStatWithEmpower.bind(null, opponent);
+  const basicAttack = (effectIndex: number, hpCost: number) => {
+    const damage = calculateEffectValue(
+      this.effects[effectIndex],
+      this.empowerLevel
+    );
+    return CommonCardAction.commonAttack(game, characterIndex, {
+      damage,
+      hpCost,
+    });
+  };
 
-    const basicAttack = (effectIndex: number, hpCost: number) => {
-        const damage = calculateEffectValue(this.effects[effectIndex], this.empowerLevel);
-        return CommonCardAction.commonAttack(game, characterIndex, { damage, hpCost });
-    };
+  return {
+    selfIndex: characterIndex,
+    self,
+    flatSelfStat,
+    selfStat,
+    basicAttack,
 
-    return {
-        selfIndex: characterIndex,
-        self,
-        flatSelfStat,
-        selfStat,
-        basicAttack,
+    name: self.name,
+    reflexive: self.cosmetic.pronouns.reflexive,
+    possessive: self.cosmetic.pronouns.possessive,
 
-        name: self.name,
-        reflexive: self.cosmetic.pronouns.reflexive,
-        possessive: self.cosmetic.pronouns.possessive,
+    opponent,
+    flatOpponentStat,
+    opponentStat,
 
+    messageCache,
+    sendToGameroom,
 
-        opponent,
-        flatOpponentStat,
-        opponentStat,
-
-        messageCache,
-        sendToGameroom,
-
-        game,
-    }
+    game,
+  };
 }
