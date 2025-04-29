3c97583c56f3026875df701b2b992bbdc3e40c09
steampunkWizard <elijahanelson2nd@gmail.com>
2025-04-29 06:36:10 -0400

    save progress

--- src/commands/tcgPlayer/playerHandlers/matchHandler.ts
+++ src/commands/tcgPlayer/playerHandlers/matchHandler.ts
@@ -1,9 +1,16 @@
-import { ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
+import {
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  ComponentType,
+  ButtonStyle,
+} from "discord.js";
 import prismaClient from "@prismaClient";
 import {
   LazyPaginatedMessage,
   PaginatedMessageMessageOptionsUnion,
 } from "@sapphire/discord.js-utilities";
+import { charWithEmoji } from "@src/tcg/formatting/emojis";
+import { CharacterName } from "@src/tcg/characters/metadata/CharacterName";
 
 const PAGE_SIZE = 12;
 
@@ -14,6 +21,9 @@ export default async function handleMatchHistory(
 
   const matches = await prismaClient.match.findMany({
     where: {
+      ladderReset: {
+        endDate: null,
+      },
       OR: [
         {
           winner: {
@@ -67,34 +77,44 @@ export default async function handleMatchHistory(
   }
 
   const pages = chunks.map((chunk, pageIndex) => {
-	const description = chunk
-	  .map((match, matchIndex) => {
-		const globalIndex = pageIndex * PAGE_SIZE + matchIndex;
-		const matchNumber = matches.length - globalIndex;
-		const { winnerId, winnerCharacter, loserCharacter, finishedAt, winner, loser } = match;
-		const won = winnerId.toString() === player.id;
-		const result = won ? "Won" : "Lost";
-		const character = won ? winnerCharacter.name : loserCharacter.name;
-		const opponent = `<@${won ? loser.discordId : winner.discordId}>`;
-		const opponentCharacter = won ? loserCharacter.name : winnerCharacter.name;
-		const timestamp = `<t:${Math.floor(new Date(finishedAt).getTime() / 1000)}:R>`;
-  
-		return `${matchNumber}. ${result} with ${character} against ${opponent} as ${opponentCharacter} ${timestamp}`;
-	  })
-	  .join("\n");
-  
-	const embed = new EmbedBuilder()
-	  .setTitle(`${player.username}'s Match History`)
-	  .setColor("Blurple")
-	  .setDescription(description);
-  
-	const page: PaginatedMessageMessageOptionsUnion = {
-	  embeds: [embed],
-	};
-  
-	return page;
+    const description = chunk
+      .map((match, matchIndex) => {
+        const index = pageIndex * PAGE_SIZE + matchIndex;
+        const matchNumber = matches.length - index - 1;
+        const { winnerCharacter, loserCharacter, finishedAt, winner, loser } =
+          match;
+        const won = winner.discordId === player.id;
+        const result = `**${won ? "Won" : "Lost"}**`;
+        const character = charWithEmoji(
+          (won ? winnerCharacter.name : loserCharacter.name) as CharacterName
+        );
+        const opponent = `<@${won ? loser.discordId : winner.discordId}>`;
+        const opponentCharacter = charWithEmoji(
+          (won ? loserCharacter.name : winnerCharacter.name) as CharacterName
+        );
+        const timestamp = `<t:${Math.floor(new Date(finishedAt).getTime() / 1000)}:R>`;
+
+        return `${matchNumber}. ${result} with ${character}  ${timestamp}\n against ${opponent} as ${opponentCharacter}`;
+      })
+      .join("\n");
+
+    const embed = new EmbedBuilder()
+      .setTitle(`${player.displayName}'s Match History`)
+      .setColor("Blurple")
+      .setDescription(description);
+
+    const page: PaginatedMessageMessageOptionsUnion = {
+      embeds: [embed],
+    };
+
+    return page;
   });
 
   const paginated = new LazyPaginatedMessage({ pages });
+  paginated.actions.forEach((action) => {
+    if (action.type === ComponentType.Button) {
+      action.style = ButtonStyle.Secondary;
+    }
+  });
   await paginated.run(interaction);
 }
