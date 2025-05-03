af6d89a1830d77efbe3d06c3979e07d4d03ebbdd
TheOreoTM <qwertzybad@gmail.com>
2025-05-03 17:56:14 +0500

    fix: add required changes

--- src/util/formatting/links.ts
+++ src/util/formatting/links.ts
@@ -1,3 +1,5 @@
+const FRIEREN_DISCORD_SERVER_AUBERST_CHANNEL_ID = "738445835234181211";
+
 export function buildThreadLink(threadId: string): string {
-  return `https://discord.com/channels/738445835234181211/${threadId}`;
+  return `https://discord.com/channels/${FRIEREN_DISCORD_SERVER_AUBERST_CHANNEL_ID}/${threadId}`;
 }
