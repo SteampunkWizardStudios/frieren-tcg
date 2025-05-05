71d9d14d89d49e2cdbc604997b45b298281c945c
TheOreoTM <qwertzybad@gmail.com>
2025-05-05 23:41:19 +0500

    feat: move preferences to seperate command

--- src/commands/tcgPlayer/tcgPlayer.ts
+++ src/commands/tcgPlayer/tcgPlayer.ts
@@ -7,8 +7,6 @@ import {
 import type { Command } from "@src/types/command";
 import handlePlayerProfile from "./playerHandlers/profileHandler";
 import handleMatchHistory from "./playerHandlers/matchHandler";
-import { handlePlayerPreferences } from "./playerHandlers/preferencesHandler";
-import { CHAR_OPTIONS, MAX_TEXT_SPEED, MIN_TEXT_SPEED } from "@src/constants";
 
 export const command: Command<ChatInputCommandInteraction> = {
   data: new SlashCommandBuilder()
@@ -44,64 +42,9 @@ export const command: Command<ChatInputCommandInteraction> = {
               "The player to get the match history of, defaults to yourself"
             )
         )
-    )
-    .addSubcommandGroup((subcommand) =>
-      subcommand
-        .setName("preferences")
-        .setDescription("Manage your preferences")
-        .addSubcommand((subcommand) =>
-          subcommand
-            .setName("view")
-            .setDescription("View your current player preferences")
-        )
-        .addSubcommand((subcommand) =>
-          subcommand
-            .setName("set-text-speed")
-            .setDescription("Set your preferred TCG text display speed")
-            .addIntegerOption((option) =>
-              option
-                .setName("speed")
-                .setDescription(
-                  "The text speed (e.g., 500 for fast, 2000 for slow - in milliseconds)"
-                )
-                .setRequired(true)
-                .setMinValue(MIN_TEXT_SPEED)
-                .setMaxValue(MAX_TEXT_SPEED)
-            )
-        )
-        .addSubcommand((subcommand) =>
-          subcommand
-            .setName("toggle-favorite-character")
-            .setDescription(
-              "Add or remove a character from your list of favourite characters"
-            )
-            .addStringOption((option) =>
-              option
-                .setName("character-name")
-                .setDescription(
-                  "The name of the character to toggle as favourite"
-                )
-                .setRequired(true)
-                .addChoices(CHAR_OPTIONS)
-            )
-        )
-        .addSubcommand((subcommand) =>
-          subcommand
-            .setName("restrict-random-to-favourites")
-            .setDescription("Restrict random character selection to favourites")
-            .addBooleanOption((option) =>
-              option
-                .setName("value")
-                .setDescription(
-                  "Restrict random character selection to favourites"
-                )
-                .setRequired(true)
-            )
-        )
     ),
 
   async execute(interaction: ChatInputCommandInteraction) {
-    const subcommandGroup = interaction.options.getSubcommandGroup();
     const subcommand = interaction.options.getSubcommand();
 
     await interaction.deferReply({
@@ -121,13 +64,6 @@ export const command: Command<ChatInputCommandInteraction> = {
             await handleMatchHistory(interaction);
           }
           break;
-      }
-
-      // sub command groups
-      switch (subcommandGroup) {
-        case "preferences": {
-          await handlePlayerPreferences(interaction);
-        }
       }
     } catch (error) {
       console.log(error);
