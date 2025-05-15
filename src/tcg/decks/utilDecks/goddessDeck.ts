deb364b08468121f8795906b0a664c11f86780d3
steampunkWizard <108240403+SteampunkWizardStudios@users.noreply.github.com>
2025-05-15 12:13:52 +0000

    implement and use goddess mode

--- src/tcg/decks/utilDecks/goddessDeck.ts
+++ src/tcg/decks/utilDecks/goddessDeck.ts
@@ -1,9 +1,11 @@
 import { CHARACTER_LIST } from "@tcg/characters/characterList";
 
-export const goddessDeck = Array.from(
+const goddessDeck = Array.from(
   new Map(
     CHARACTER_LIST.flatMap(({ cards }) =>
       cards.map(({ card }) => [card.title, card])
     )
   ).values()
 );
+
+export default goddessDeck;
