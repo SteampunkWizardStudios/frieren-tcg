7a10dc19697935abdcd063b873ec4e8904f5c291
steampunkWizard <108240403+SteampunkWizardStudios@users.noreply.github.com>
2025-05-15 12:51:49 +0000

    media links example

---
+++ src/tcg/formatting/mediaLinks.ts
@@ -0,0 +1,12 @@
+const frierenMediaLinks = {
+  offensiveMagicAnalysisZoltraak_image:
+    "https://cdn.discordapp.com/attachments/1351391350398128159/1355588256267501888/Offensive_Magic_Analysis_Zoltraak.png?ex=67e97971&is=67e827f1&hm=193fb4668269bd8509f7b4ce4a092c12af7b44ac8fd5264dfac08c5da5a349bf&",
+  offensiveMagicAnalysisZoltraak_gif:
+    "https://cdn.discordapp.com/attachments/1360969158623232300/1364283548789379163/GIF_2604048789.gif?ex=68091b91&is=6807ca11&hm=a7fd85473cc4a8c441193bd990c6517e95d5cc6789023f530c27f4d21e5e70dc&",
+} as const;
+
+const mediaLinks = {
+  ...frierenMediaLinks,
+} as const;
+
+export default mediaLinks;
