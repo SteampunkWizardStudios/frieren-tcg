7212e01ad78f698aed03b7922bc2ff96f95362b8
steampunkWizard <elijahanelson2nd@gmail.com>
2025-05-16 19:35:19 -0400

    upd edel status to use hideEmpower

--- src/tcg/decks/utilDecks/edelStatuses.ts
+++ src/tcg/decks/utilDecks/edelStatuses.ts
@@ -4,9 +4,8 @@ import { CardEmoji } from "@tcg/formatting/emojis";
 
 export const sleepy = new Card({
   title: "Sleepy",
-  cardMetadata: { nature: Nature.None, temporary: true },
+  cardMetadata: { nature: Nature.None, temporary: true, hideEmpower: true },
   emoji: CardEmoji.GENERIC,
-  printEmpower: false,
   description: () =>
     "If you do not play this card, skip next turn. This card is removed from your deck after it becomes playable, regardless of it was played.",
   effects: [],
@@ -18,14 +17,13 @@ export const sleepy = new Card({
   onNotPlayed: function (this: Card, { self }) {
     self.deck.removeCard(this);
     self.skipTurn = true;
-  }
+  },
 });
 
 export const mesmerized = new Card({
   title: "Mesmerized",
-  cardMetadata: { nature: Nature.None, temporary: true },
+  cardMetadata: { nature: Nature.None, temporary: true, hideEmpower: true },
   emoji: CardEmoji.GENERIC,
-  printEmpower: false,
   description: () =>
     "If you play this card on the first turn it is playable, it is removed from your deck, otherwise, this card cannot be removed from your deck.",
   effects: [],
@@ -43,7 +41,7 @@ export const mesmerized = new Card({
     if (this.cardMetadata.temporary) {
       self.deck.removeCard(this);
     }
-  }
+  },
 });
 
 export const weakened = new Card({
@@ -65,5 +63,5 @@ export const weakened = new Card({
     [StatsEnum.ATK, StatsEnum.DEF, StatsEnum.SPD].forEach((stat) => {
       selfStat(0, stat, -1);
     });
-  }
+  },
 });
