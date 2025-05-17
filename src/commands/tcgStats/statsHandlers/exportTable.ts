572ea41d1c6933d03c4463d8cb31e25146b45aee
steampunkWizard <elijahanelson2nd@gmail.com>
2025-05-17 09:17:50 -0400

    remove console.logs in exportTable.ts

--- src/commands/tcgStats/statsHandlers/exportTable.ts
+++ src/commands/tcgStats/statsHandlers/exportTable.ts
@@ -26,8 +26,6 @@ export default async function exportTable(
     return;
   }
 
-  console.log(`Data for table \`${table}\`:`, data);
-
   try {
     if (!Array.isArray(data)) {
       throw new Error(
@@ -36,11 +34,9 @@ export default async function exportTable(
     }
 
     const csvData = stringify(data, { header: true });
-	console.log(`CSV data for table \`${table}\`:`, csvData);
     const attachment = new AttachmentBuilder(Buffer.from(csvData, "utf-8"), {
       name: `${table}.csv`,
     });
-	console.log(`Attachment created for table \`${table}\`:`, attachment);	
 
     await interaction.editReply({
       content: `Data for table \`${table}\`:`,
