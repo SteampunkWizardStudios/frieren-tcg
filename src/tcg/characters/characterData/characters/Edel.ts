811f2b8c3600cd56eb233add4d05bbf72da2989d
steampunkWizard <elijahanelson2nd@gmail.com>
2025-05-18 08:45:10 -0400

    edel tweaks + unhide

--- src/tcg/characters/characterData/characters/Edel.ts
+++ src/tcg/characters/characterData/characters/Edel.ts
@@ -8,10 +8,10 @@ import { TCGThread } from "@src/tcgChatInteractions/sendGameMessage";
 import mediaLinks from "@src/tcg/formatting/mediaLinks";
 
 const edelStats = new Stats({
-  [StatsEnum.HP]: 70,
-  [StatsEnum.ATK]: 7,
-  [StatsEnum.DEF]: 7,
-  [StatsEnum.SPD]: 10,
+  [StatsEnum.HP]: 80,
+  [StatsEnum.ATK]: 8,
+  [StatsEnum.DEF]: 9,
+  [StatsEnum.SPD]: 11,
   [StatsEnum.Ability]: 0,
 });
 
@@ -26,17 +26,23 @@ const Edel = new CharacterData({
   stats: edelStats,
   cards: edelDeck,
   ability: {
-    abilityName: "Memory Transference Specialist",
-    abilityEffectString: `At the start of each turn, see a random card from your opponent's hand, and lower its empowerment by 1
-
-    **Sub-Ability: A Superior Opponent** - While you make Eye Contact with the opponent, all your moves have Priority+1.
-    `,
+    abilityName: "A Superior Opponent",
+    abilityEffectString:
+      "While you make Eye Contact with the opponent, all your moves have Priority+1",
+    subAbilities: [
+      {
+        name: "Memory Transference Specialist",
+        description: `At the start of each turn, see a random card from your opponent's hand, and lower its empowerment by 1`,
+      },
+    ],
     abilityStartOfTurnEffect: (game, characterIndex, messageCache) => {
       const self = game.getCharacter(characterIndex);
       const opponent = game.getCharacter(1 - characterIndex);
 
+      // Memory Transference Specialist
       const randomCard =
         opponent.hand[Math.floor(Math.random() * opponent.hand.length)];
+      randomCard.empowerLevel -= 1;
 
       const selfThread =
         characterIndex === 0
@@ -49,7 +55,36 @@ const Edel = new CharacterData({
       );
       messageCache.push(`${randomCard.printCard()}`, selfThread);
 
-      // Sub-Ability: A Superior Opponent
+      // QOL counters
+      messageCache.push(
+        `${self.name} forced ${opponent.name} to discard **${opponent.additionalMetadata.forcedDiscards}** cards.`,
+        selfThread
+      );
+      // count number of status cards in opponent's deck
+      const check = [...opponent.deck.activePile, ...opponent.deck.discardPile];
+      const [sleepyCount, mesmerizedCount, weakenedCount] = check.reduce(
+        (acc: [number, number, number], card) => {
+          switch (card.title) {
+            case "Sleepy":
+              acc[0]++;
+              break;
+            case "Mesmerized":
+              acc[1]++;
+              break;
+            case "Weakened":
+              acc[2]++;
+              break;
+          }
+          return acc;
+        },
+        [0, 0, 0]
+      );
+      messageCache.push(
+        `${opponent.name} has ${sleepyCount} Sleepy, ${mesmerizedCount} Mesmerized, and ${weakenedCount} Weakened in their deck.`,
+        selfThread
+      );
+
+      // A Superior Opponent
       if (self.stats.stats.Ability > 0) {
         self.stats.stats.Ability--;
 
@@ -65,9 +100,6 @@ const Edel = new CharacterData({
         self.ability.abilitySelectedMoveModifierEffect = undefined;
       }
     },
-  },
-  additionalMetadata: {
-    hidden: true,
   },
 });
 
