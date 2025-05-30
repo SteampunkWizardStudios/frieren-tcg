bd1f43419e762d9a9e86832fcb2fbf79d2562974
HxHexa <tomdale123456@gmail.com>
2025-05-30 12:11:24 -0400

    let stark attack be uninterruptable by sol

--- src/tcg/decks/StarkDeck.ts
+++ src/tcg/decks/StarkDeck.ts
@@ -283,7 +283,7 @@ const a_eisensAxeCleave = new Card({
 export const a_lastStand = new Card({
   title: "Last Stand",
   description: ([dmg]) =>
-    `DEF-5 for 2 turns. This character's HP cannot drop below 1 for 2 turns. At the end of next turn, HP-20, use 2 Resolves, strike for DMG ${dmg}.`,
+    `DEF-5 for 2 turns. This character's HP cannot drop below 1 for 2 turns. At the end of next turn, HP-20, use 2 Resolves, strike for DMG ${dmg}. This attack cannot be interrupted by Sorganeil.`,
   emoji: CardEmoji.STARK_CARD,
   cardMetadata: { nature: Nature.Attack, signature: true },
   effects: [25],
@@ -303,9 +303,9 @@ export const a_lastStand = new Card({
     game.characters[characterIndex].timedEffects.push(
       new TimedEffect({
         name: "Impending Lightning",
-        description: `Strike for ${damage} damage.`,
+        description: `Strike for ${damage} damage. Uninterruptable.`,
         turnDuration: 2,
-        metadata: { removableBySorganeil: true },
+        metadata: { removableBySorganeil: false },
         executeEndOfTimedEffectActionOnRemoval: false,
         activateEndOfTurnActionThisTurn: false,
         endOfTimedEffectAction: (game, characterIndex) => {
