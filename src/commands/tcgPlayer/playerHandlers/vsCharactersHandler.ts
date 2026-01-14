073a292f4bab165c2a955bab5cc61728d81c841d
steampunkWizard <elijahanelson2nd@gmail.com>
2026-01-14 07:00:42 -0500

    fix vsCharactersHandler

--- src/commands/tcgPlayer/playerHandlers/vsCharactersHandler.ts
+++ src/commands/tcgPlayer/playerHandlers/vsCharactersHandler.ts
@@ -2,60 +2,28 @@ import { ChatInputCommandInteraction } from "discord.js";
 import { formatMatchHistoryPages } from "./formatMatchHistoryPages";
 import { getWinrate } from "@src/util/utils";
 import { getMatchHistoryAgainstCharacter } from "@src/util/db/getMatchHistory";
-<<<<<<< HEAD:src/commands/tcgPlayer/playerHandlers/vsCharactersHandler.ts
-=======
-import handleVsCharacterRecordOverview from "./handleVsCharacterOverview";
-import querySeason from "@src/util/db/querySeason";
 import { charWithEmoji } from "@tcg/formatting/emojis";
->>>>>>> a940d0a62aaf6eba231712e02dcd5ca8c686ba4a:src/commands/tcgPlayer/playerHandlers/vsCharacterHandler.ts
 import { CharacterName } from "@tcg/characters/metadata/CharacterName";
 
 export async function handleVsCharacter(
   interaction: ChatInputCommandInteraction
 ) {
-<<<<<<< HEAD:src/commands/tcgPlayer/playerHandlers/vsCharactersHandler.ts
-  const playerId = interaction.options.getUser("player") ?? interaction.user;
+  const player = interaction.options.getUser("player") ?? interaction.user;
   const playerCharacter: CharacterName =
     (interaction.options.getString("character") as CharacterName) ??
     CharacterName.Frieren;
   const oppCharacter: CharacterName =
     (interaction.options.getString("character") as CharacterName) ??
     CharacterName.Frieren;
-=======
-  const player = interaction.options.getUser("player") ?? interaction.user;
-  const playerCharacter = interaction.options.getString("player-character");
-  const oppCharacter = interaction.options.getString("opponent-character");
-  const seasonId = interaction.options.getInteger("season");
-  const seasonQuery = seasonId ? await querySeason(seasonId) : null;
-
-  if (!oppCharacter) {
-    return handleVsCharacterRecordOverview(
-      player,
-      playerCharacter,
-      interaction,
-      seasonQuery
-    );
-  }
-
->>>>>>> a940d0a62aaf6eba231712e02dcd5ca8c686ba4a:src/commands/tcgPlayer/playerHandlers/vsCharacterHandler.ts
   const vsCharacterMatches = await getMatchHistoryAgainstCharacter(
     player.id,
     playerCharacter,
-<<<<<<< HEAD:src/commands/tcgPlayer/playerHandlers/vsCharactersHandler.ts
-    oppCharacter);
-    
-  if (vsCharacterMatches.length === 0) {
-    await interaction.editReply({
-      content: `There are no records of ${playerId} against ${oppCharacter} in the current ladder.`,
-=======
-    oppCharacter,
-    seasonQuery
+    oppCharacter
   );
 
   if (vsCharacterMatches.length === 0) {
     await interaction.editReply({
-      content: `There are no records of this matchup in the selected ladder.`,
->>>>>>> a940d0a62aaf6eba231712e02dcd5ca8c686ba4a:src/commands/tcgPlayer/playerHandlers/vsCharacterHandler.ts
+      content: `There are no records of ${player.id} against ${oppCharacter} in the current ladder.`,
     });
     return;
   }
